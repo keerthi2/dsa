1ebf7ba2289386784bcbe4788149555c583b6ed7
keerthi <keerthi.hv22@gmail.com>
2026-03-24 13:44:33 +0530

    Added implementation for Missing range of Numbers using better approach

---
+++ src/main/java/TopHundredDSA/Array/Easy/MissingRangeOfNumbers.java
@@ -0,0 +1,41 @@
+package TopHundredDSA.Array.Easy;
+
+import java.util.ArrayList;
+
+// Source : https://www.geeksforgeeks.org/dsa/missing-ranges-of-numbers/
+public class MissingRangeOfNumbers {
+    static int arr[] = {-48, -10, -6, -4, 0, 4, 17};
+    static int lower = -54, upper = 17;
+
+    //Output:[[-54, -49], [-47, -11], [-9, -7], [-5, -5], [-3, -1], [1, 3], [5,16]]
+    static void main(String[] args) {
+        missingRangeOfNumbers(arr, lower, upper);
+    }
+
+    public static ArrayList<int[]> missingRangeOfNumbers(int[] arr, int lowerNumber, int upperNumber) {
+        int lower = lowerNumber;
+        int upper = upperNumber;
+        ArrayList<int[]> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > lower) {
+                int[] temp = new int[2];
+                temp[0] = lower;
+                temp[1] = arr[i] - 1;
+                list.add(temp);
+            }
+            lower = arr[i] + 1;
+
+        }
+
+        if (arr[arr.length - 1] < upper) {
+            int[] temp = new int[2];
+            temp[0] = lower;
+            temp[1] = upper;
+            list.add(temp);
+        }
+
+        return list;
+
+    }
+}
